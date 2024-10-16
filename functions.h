583447b63299101b5542d630ba4e1664a9fa1ded
12401453 <12401453@protonmail.com>
2024-10-16 02:11:33 +0100

    formatting

--- functions.h
+++ functions.h
@@ -1,69 +1,87 @@
+bool Sniff(std::string cyr_id, std::string substr, short int substr_length)
+{
+  substr_length = substr_length * 2;
+  if ((substr_length + 3) >= cyr_id.length())
+  {
+    if (cyr_id.find(substr) == -1)
+    {
+      return false;
+    }
 
+    else
+    {
+      return true;
+    }
+  }
+  else
+  {
+    std::ofstream testFile;
+    testFile.open("test.txt", std::ios_base::app);
+    testFile << cyr_id << '|' << substr << std::endl;
 
+    int end_pos = cyr_id.length();
+    int offset_pos = 0;
+    if (end_pos % 2 == 1)
+    {
+      offset_pos = end_pos - substr_length - 3;
+    }
+    else
+    {
+      offset_pos = end_pos - substr_length - 2;
+    }
+    std::string sniffstack = cyr_id.replace(0, offset_pos, "");
 
+    testFile << end_pos << '|' << offset_pos << std::endl
+             << sniffstack << '|' << substr << std::endl;
 
-bool Sniff(std::string cyr_id, std::string substr, short int substr_length) {
-      substr_length = substr_length * 2;
-      if((substr_length + 3) >= cyr_id.length()) {
-        if(cyr_id.find(substr) == -1) { return false;}
+    testFile.close();
 
-        else { return true;}
-      }
-    else {
-                      std::ofstream testFile;
-                      testFile.open("test.txt", std::ios_base::app);
-                      testFile << cyr_id << '|' << substr << std::endl;
-
-
-     int end_pos = cyr_id.length();
-     int offset_pos = 0;
-    if(end_pos % 2 == 1) {offset_pos = end_pos - substr_length -3;}
-    else {offset_pos = end_pos - substr_length -2;}
-     std::string sniffstack = cyr_id.replace(0, offset_pos, "");
-
-      testFile << end_pos << '|' << offset_pos << std::endl << sniffstack << '|' << substr << std::endl;
-
-
-
-                      testFile.close();
-
-     if(sniffstack.find(substr) == -1) { return false; }
-     else {return true;}
+    if (sniffstack.find(substr) == -1)
+    {
+      return false;
+    }
+    else
+    {
+      return true;
+    }
   }
 }
 
-std::string Fetch_Article(int row_no) {
- int outer_map_no = 51;
- std::string article = Nom_[outer_map_no][row_no];
- return article;
-
+std::string Fetch_Article(int row_no)
+{
+  int outer_map_no = 51;
+  std::string article = Nom_[outer_map_no][row_no];
+  return article;
 }
 
- void pisanie_clean(std::string &verb) {
+void pisanie_clean(std::string &verb)
+{
 
+  std::string str1, str2;
+  int x, y;
+  for (int letno = 0; letno < 1; letno++)
+  {
 
-         std::string str1, str2;
-         int x, y;
-                 for (int letno = 0; letno < 1; letno++) {
+    switch (letno)
+    {
 
-                switch (letno) {
+    case 0:
+      str1.assign("pьs");
+      str2.assign("pis");
+      break;
+    }
 
-                  case 0:
-                     str1.assign("pьs");
-                     str2.assign("pis");
-                     break;
+    do
+    {
+      x = verb.find(str1);
+      if (x == -1)
+      {
+        break;
+      }
+      y = str1.length();
+      verb.replace(x, y, str2);
+    } while (x != -1);
+  }
 
-                 }
-
-                do {
-            x = verb.find(str1);
-                if (x == -1)  {break;}
-                y = str1.length();
-                verb.replace(x, y, str2); }
-                while (x != -1);
-
-
-              }
-
-              return;
-         }
+  return;
+}
--- main.cpp
+++ main.cpp
@@ -40,20 +40,15 @@ struct Lemma {
 
 
 
- friend bool operator== (const Lemma &l1, const Lemma &l2);
- friend bool operator< (const Lemma &l1, const Lemma &l2);
+  friend bool operator== (const Lemma &l1, const Lemma &l2);
+  friend bool operator< (const Lemma &l1, const Lemma &l2);
 
 
- Lemma(long int lemma_id_, std::string lemma_form_ = "", std::string morph_replace_ = "", std::string second_velar_ = "",
- std::string poss_doublet_ = "", short int loan_place_ = 0, short int long_adj_ = 0, bool non_assim_ = false, std::string eng_trans_ = "", std::string pre_jot_ = "",
- std::string etym_disc_ = "",  bool bad_etym_ = false,
- std::string loan_source_ = "", std::string conj_type_ = "", short int noun_verb_ = 0)
-        : lemma_id{ lemma_id_ }, lemma_form{ lemma_form_ }, morph_replace{ morph_replace_ }, second_velar{ second_velar_ }, poss_doublet { poss_doublet_ },
-        loan_place { loan_place_}, long_adj { long_adj_ }, non_assim { non_assim_ }, eng_trans { eng_trans_ }, pre_jot { pre_jot_ },
-        etym_disc { etym_disc_ }, bad_etym { bad_etym_ }, loan_source { loan_source_ }, conj_type { conj_type_ }, noun_verb {noun_verb_}
-    {
+  Lemma(
+    long int lemma_id_, std::string lemma_form_ = "", std::string morph_replace_ = "", std::string second_velar_ = "", std::string poss_doublet_ = "", short int loan_place_ = 0, short int long_adj_ = 0, bool non_assim_ = false, std::string eng_trans_ = "", std::string pre_jot_ = "", std::string etym_disc_ = "",  bool bad_etym_ = false, std::string loan_source_ = "", std::string conj_type_ = "", short int noun_verb_ = 0
+    ) : lemma_id{ lemma_id_ }, lemma_form{ lemma_form_ }, morph_replace{ morph_replace_ }, second_velar{ second_velar_ }, poss_doublet { poss_doublet_ }, loan_place { loan_place_}, long_adj { long_adj_ }, non_assim { non_assim_ }, eng_trans { eng_trans_ }, pre_jot { pre_jot_ }, etym_disc { etym_disc_ }, bad_etym { bad_etym_ }, loan_source { loan_source_ }, conj_type { conj_type_ }, noun_verb {noun_verb_}{
 
-    }
+  }
 
 };
 bool operator== (const Lemma &l1, const Lemma &l2)
@@ -66,11 +61,13 @@ bool operator< (const Lemma &l1, const Lemma &l2)
 }
 
 std::string str_Truncate(std::string str1, int lop_off) {
-int end_pos = str1.length();
-int cut_pos = end_pos - lop_off;
-std::string str_cut = str1.replace(cut_pos, lop_off, "");
-return str_cut;
-                                   }
+  
+  int end_pos = str1.length();
+  int cut_pos = end_pos - lop_off;
+  std::string str_cut = str1.replace(cut_pos, lop_off, "");
+  return str_cut;
+                                   
+}
 
 #include "lemlist_2.h"
 #include "conj_type_enum.h"
@@ -84,160 +81,159 @@ return str_cut;
 
 
 std::string Dejotate(std::string cs_word) {
+  
+  std::string str1, str2;
+  int x, y;
+  for (int letno = 0; letno < 33; letno++) {
+
+    switch (letno) {
+
+      case 0:
+        str1.assign("strj");
+        str2.assign("šћŕ");
+        break;
+      case 1:
+        str1.assign("stj");
+        str2.assign("šћ");
+        break;
+      case 2:
+        str1.assign("zdj");
+        str2.assign("žђ");
+        break;
+      case 3:
+        str1.assign("slj");
+        str2.assign("šĺ");
+        break;
+      case 4:
+        str1.assign("zlj");
+        str2.assign("žĺ");
+        break;
+      case 5:
+        str1.assign("znj");
+        str2.assign("žń");
+        break;
+      case 6:
+        str1.assign("snj");
+        str2.assign("šń");
+        break;
+      case 7:
+        str1.assign("trj");
+        str2.assign("ћŕ");
+        break;
+      case 8:
+        str1.assign("drj");
+        str2.assign("ђŕ");
+        break;
+      case 9:
+        str1.assign("tvj");
+        str2.assign("ћvĺ");
+        break;
+      case 10:
+        str1.assign("bj");
+        str2.assign("bĺ");
+        break;
+      case 11:
+        str1.assign("pj");
+        str2.assign("pĺ");
+        break;
+      case 12:
+        str1.assign("mj");
+        str2.assign("mĺ");
+        break;
+      case 13:
+        str1.assign("vj");
+        str2.assign("vĺ");
+        break;
+      case 14:
+        str1.assign("kt");
+        str2.assign("ћ");
+        break;
+      case 15:
+        str1.assign("gt");
+        str2.assign("ћ");
+        break;
+      case 16:
+        str1.assign("tj");
+        str2.assign("ћ");
+        break;
+      case 17:
+        str1.assign("dj");
+        str2.assign("ђ");
+        break;
+      case 18:
+        str1.assign("nj");
+        str2.assign("ń");
+        break;
+      case 19:
+        str1.assign("lj");
+        str2.assign("ĺ");
+        break;
+      case 20:
+        str1.assign("rj");
+        str2.assign("ŕ");
+        break;
+      case 21:
+        str1.assign("sj");
+        str2.assign("š");
+        break;
+      case 22:
+        str1.assign("zj");
+        str2.assign("ž");
+        break;
+      case 23:
+        str1.assign("čj");
+        str2.assign("č");
+        break;
+      case 24:
+        str1.assign("šj");
+        str2.assign("š");
+        break;
+      case 25:
+        str1.assign("žj");
+        str2.assign("ž");
+        break;
+      case 26:
+        str1.assign("zž");
+        str2.assign("žǯ");
+          break;
+      case 27:
+        str1.assign("jj");
+        str2.assign("j");
+          break;
+      case 28:
+        str1.assign("gj");
+        str2.assign("ž");
+          break;
+      case 29:
+        str1.assign("kj");
+        str2.assign("č");
+          break;
+      case 30:
+        str1.assign("xj");
+        str2.assign("š");
+          break;
+      case 31:
+        str1.assign("ђj");
+        str2.assign("ђ");
+          break;
+      case 32:
+        str1.assign("ńj");
+        str2.assign("ń");
+          break;
+    }
+
+    do {
+      x = cs_word.find(str1);
+      if (x == -1)  {break;}
+      y = str1.length();
+      cs_word.replace(x, y, str2); 
+    }
+    while (x != -1);
 
 
-        std::string str1, str2;
-        int x, y;
-                for (int letno = 0; letno < 33; letno++) {
-
-               switch (letno) {
-
-                 case 0:
-                   str1.assign("strj");
-                   str2.assign("šћŕ");
-                   break;
-                 case 1:
-                   str1.assign("stj");
-                   str2.assign("šћ");
-                   break;
-                case 2:
-                  str1.assign("zdj");
-                  str2.assign("žђ");
-                  break;
-                case 3:
-                  str1.assign("slj");
-                  str2.assign("šĺ");
-                  break;
-                case 4:
-                  str1.assign("zlj");
-                  str2.assign("žĺ");
-                  break;
-                case 5:
-                  str1.assign("znj");
-                  str2.assign("žń");
-                  break;
-                case 6:
-                  str1.assign("snj");
-                  str2.assign("šń");
-                  break;
-                case 7:
-                  str1.assign("trj");
-                  str2.assign("ћŕ");
-                  break;
-                case 8:
-                  str1.assign("drj");
-                  str2.assign("ђŕ");
-                  break;
-                case 9:
-                  str1.assign("tvj");
-                  str2.assign("ћvĺ");
-                  break;
-                case 10:
-                  str1.assign("bj");
-                  str2.assign("bĺ");
-                  break;
-                case 11:
-                  str1.assign("pj");
-                  str2.assign("pĺ");
-                  break;
-                case 12:
-                  str1.assign("mj");
-                  str2.assign("mĺ");
-                  break;
-                case 13:
-                  str1.assign("vj");
-                  str2.assign("vĺ");
-                  break;
-                case 14:
-                  str1.assign("kt");
-                  str2.assign("ћ");
-                  break;
-                case 15:
-                  str1.assign("gt");
-                  str2.assign("ћ");
-                  break;
-                case 16:
-                  str1.assign("tj");
-                  str2.assign("ћ");
-                  break;
-                case 17:
-                  str1.assign("dj");
-                  str2.assign("ђ");
-                  break;
-                case 18:
-                  str1.assign("nj");
-                  str2.assign("ń");
-                  break;
-                case 19:
-                  str1.assign("lj");
-                  str2.assign("ĺ");
-                  break;
-                case 20:
-                  str1.assign("rj");
-                  str2.assign("ŕ");
-                  break;
-                case 21:
-                  str1.assign("sj");
-                  str2.assign("š");
-                  break;
-                case 22:
-                  str1.assign("zj");
-                  str2.assign("ž");
-                  break;
-                case 23:
-                  str1.assign("čj");
-                  str2.assign("č");
-                  break;
-                case 24:
-                  str1.assign("šj");
-                  str2.assign("š");
-                  break;
-                case 25:
-                  str1.assign("žj");
-                  str2.assign("ž");
-                  break;
-                case 26:
-                  str1.assign("zž");
-                  str2.assign("žǯ");
-                    break;
-                case 27:
-                  str1.assign("jj");
-                  str2.assign("j");
-                    break;
-                case 28:
-                  str1.assign("gj");
-                  str2.assign("ž");
-                    break;
-                case 29:
-                  str1.assign("kj");
-                  str2.assign("č");
-                    break;
-                case 30:
-                  str1.assign("xj");
-                  str2.assign("š");
-                    break;
-                case 31:
-                  str1.assign("ђj");
-                  str2.assign("ђ");
-                    break;
-                case 32:
-                  str1.assign("ńj");
-                  str2.assign("ń");
-                    break;
-                }
-
-               do {
-           x = cs_word.find(str1);
-               if (x == -1)  {break;}
-               y = str1.length();
-               cs_word.replace(x, y, str2); }
-               while (x != -1);
-
-
-             }
-
-             return cs_word;
-        }
+  }
+  return cs_word;
+}
 
 
 std::string deep_clean_Cyr(std::string form_cell) {
